6b8633cc036d5c379fab8779b11f0ab736f3796d
unknown <rafael@Rafael-PC.ntm>
2020-01-08 15:56:53 -0300

    FXML teste

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,31 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	
+
 	// inside the primaryStage stage we will have several scenes
 	public void start(Stage primaryStage) {
+		
+		// Parent == super class of AnchorPane
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
