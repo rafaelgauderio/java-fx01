64d4fed96afe6c000aa6686dc6ded9bb81fe8db5
unknown <rafael@Rafael-PC.ntm>
2020-01-09 09:42:54 -0300

    Creating Alerts

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 //class that control the scene
@@ -13,7 +15,10 @@ public class ViewController {
 	@FXML
 	public void onBtTestAction() {
 		
+		Alerts.showAlert("Alert title: WARNING", null,"Use a secure password: with letters, numbers,"
+				+ " alpha numerals, and at least one uppercase character!", AlertType.WARNING);
 		System.out.println("You have click on the button!");
+		
 		
 	}
 
